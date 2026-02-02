b7e4621b212f13acd303b0be1f820a0b11fbe32f
befjune <may.renuka.ch@gmail.com>
2026-02-02 08:04:00 +0000

    week 4 exercise1

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, const char * argv[]) {
     int c=5;
-    struct node a,b,*head ;
+    struct node a,b,d,e,*head ;
     a.value = c;
     a.next=&b;
     head=&a;
@@ -19,12 +19,48 @@ int main(int argc, const char * argv[]) {
 
     printf("%d\n", head ->value ); //what value for 5
     printf("%d\n", head ->next->value ); //what value for 8
+    printf("---------------ex.1---------------\n");
+    
+    //ex.1
+    b.next=&d;
+    head = &b;
+    d.value = head->value+3;
+    printf("%d\n", head ->next->value ); //what value for 11
+
+    /*d.next= NULL;
+    head = &d;
+    e.value = head -> value+3;
+    printf("%d\n", head ->next->value );*/
+
 /*  Exercise I
     1. Add 1 more than at the end
     2. Add value(11)
     3. Make next become NULL
  */
-    
+
+    //insert beginning
+    struct node f;
+    f.value = 2;
+    f.next = &a;
+    head = &f; //เอา head มาชี้ให้ f เป็นตัวหน้าสุด
+    printf("%d\n", head ->value ); //what value for 5
+    printf("%d\n", head ->next->value ); //what value for 8
+    printf("%d\n", head ->next->next->value ); //what value for 8
+    printf("%d\n", head ->next->next->next->value ); //what value for 8
+    printf("----------------ex.2-----------------\n");
+
+    //insert between
+    struct node g;
+    g.value = 7;
+    g.next = &b;
+    a.next = &g;
+    head = &f;
+    printf("%d\n", head ->value ); //what value for 5
+    printf("%d\n", head ->next->value ); //what value for 8
+    printf("%d\n", head ->next->next->value ); //what value for 8
+    printf("%d\n", head ->next->next->next->value ); //what value for 8
+    printf("%d\n", head ->next->next->next->next->value ); //what value for 8
+
 /*  Exercise II
         1. Add 1 more than at the begining!!!!
         2. Add value (2)
